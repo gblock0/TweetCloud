aba44db96ef96044eb455e9a400647acc2064cd2
Greg Block <gblock0@gmail.com>
2020-07-31 18:07:11 -0700

    Format test.py

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,8 @@
 from tweetcloud import helpers
 
+
 def test_normalize():
     tweet = "test words test@email.com https://test.co nuke some other nuke words"
-    words = {"words": 1, "gum":5}
+    words = {"words": 1, "gum": 5}
     helpers.normalize_and_split_tweet(tweet, words)
-    assert words == {"words":3, "test": 1, "gum":5, "nuke": 2}
+    assert words == {"words": 3, "test": 1, "gum": 5, "nuke": 2}
