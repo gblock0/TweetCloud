c88b0207db07d6121d269a9c469472f6ead1b4fd
Greg Block <gblock0@gmail.com>
2020-08-01 14:56:22 -0700

    Use hp_otp to seed the word sets and generate new word sets/known test images

--- tests/helpers/generate_test_word_dictionaries.py
+++ tests/helpers/generate_test_word_dictionaries.py
@@ -1,13 +1,20 @@
 # Use the set of English words containing lower-case letters with no punctuation
 import random
+import re
 
-from english_words import english_words_lower_alpha_set
-
+possible_words = []
 
 def generate_word_set(num_words, file_path):
     dic = {}
     for _ in range(num_words):
-        random_word = random.sample(english_words_lower_alpha_set, 1)[0]
+        found_unique_word = False
+        random_word = ""
+        while not found_unique_word:
+            # only grab letters
+            random_word = re.sub(r"[\W_]+", "", random.sample(possible_words, 1)[0])
+            if random_word not in dic:
+                found_unique_word = True
+
         dic[random_word] = random.randint(2, 25)
 
     # Save the dictionary to a txt file for the test to read
@@ -15,6 +22,9 @@ def generate_word_set(num_words, file_path):
     f.write(str(dic))
     f.close()
 
+
+otp = open("tests/word_sets/hp_otp.txt", "r")
+possible_words = set(otp.read().lower().split())
 
 # Generate new word sets
 #  NOTE: Committing these will fail the test_create_wordcloud test
