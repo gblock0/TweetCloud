69877cd691fecb417fcfa5d6c74038c8c3a1f35f
niral <the1mightypan@gmail.com>
2020-08-02 14:43:13 -0700

    update usage and CLI help text

--- tweetcloud/app.py
+++ tweetcloud/app.py
@@ -8,11 +8,32 @@ from tweetcloud.helpers import text as texthelper
 from tweetcloud.helpers import twitter_api
 from tweetcloud.visualization import wordcloud
 
+MAX_TWEETS = 3000
+
 
 @click.command()
-@click.option("-n", "--number-of-tweets", type=click.IntRange(0, 3000), default=1000)
+@click.option(
+    "-n",
+    "--number-of-tweets",
+    type=click.IntRange(0, MAX_TWEETS),
+    default=1000,
+    show_default=True,
+    metavar="",
+    help=(
+        f"Number of tweets to use in total. Currently, TweetCloud "
+        f"does not support more than {MAX_TWEETS} tweets."
+        f"\n\nNote that the tweets are grouped by time, not count, so "
+        f"using this option may not affect the frames in the GIF."
+    ),
+)
 @click.argument("screen_name", type=click.STRING)
 def cli(screen_name: str, number_of_tweets: int):
+    """
+    Generate animated series of word clouds from SCREEN_NAME's tweets.
+
+    Example: tweetcloud nasa
+    """
+
     # Connect to the Twitter API
     session = twitter.Api(
         consumer_key=os.getenv("TWTR_CONS_KEY"),
